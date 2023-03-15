2eca3ab9a848c686976aaf0576f2f477ead22c70
yelikang <530331059@qq.com>
2023-03-15 18:38:28 +0800

    webpack源码

--- debug/start/modules/user.js
+++ debug/start/modules/user.js
@@ -4,9 +4,9 @@
  * @Date: 2022-12-06 17:33:59
  */
 import sex from './sex'
+import Style from './style.css'
 const user = {
     age: 30,
     type: sex.sex
 }
-
 export default user
--- debug/start/webpack.config.js
+++ debug/start/webpack.config.js
@@ -3,8 +3,10 @@
  * @Autor: Yelikang
  * @Date: 2022-12-06 15:03:11
  */
+
 const path = require("path");
-const TestPlugin = require("./plugin/test.js");
+// const TestPlugin = require("./plugin/test.js");
+const DependencyPlugin = require("./plugin/DependencePlugin");
 
 module.exports = {
 	// 解析上下文路径为当前webpack.config.js所处路径
@@ -16,8 +18,23 @@ module.exports = {
 		path: path.resolve(__dirname, "dist")
 	},
 	plugins: [
-		new TestPlugin({
-			name: "哈哈哈"
+		// new TestPlugin({
+		// 	name: "哈哈哈"
+		// }),
+		new DependencyPlugin({
+			dirPath: 'modules'
 		})
-	]
+	],
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: [
+					{
+						loader: "css-loader"
+					}
+				]
+			}
+		]
+	}
 };
